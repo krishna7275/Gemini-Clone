db58d06ad4dd9906fbcc4e4e1ad4bd22b349f5e3
krishna7275 <kpy.yt9833067464@gmail.com>
2025-12-02 21:19:33 +0530

    ok UI is complte

---
+++ Gemini-Clone/src/Config/Gemini.js
@@ -0,0 +1,16 @@
+GEMINI_API_KEY = "AIzaSyAHla15ZALwcFIEXm-ZrJbBGDZNy6sPBuo"
+
+import { GoogleGenAI } from "@google/genai";
+
+// The client gets the API key from the environment variable `GEMINI_API_KEY`.
+const ai = new GoogleGenAI({});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
+}
+
+main();
